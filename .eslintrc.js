e3038b1356a2ab4c3ace54707b87f442715706c0
pustylnikov <yura.pustylnikov@gmail.com>
2020-05-21 12:19:50 +0300

    initial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+    root: true,
+    extends: '@react-native-community',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['dist/', 'node_modules/'],
+    rules: {
+        'prettier/prettier': 0,
+        'indent': [2, 4, {'SwitchCase': 1}],
+        'semi': [2, 'always'],
+        'comma-dangle': [2, 'always-multiline'],
+        'no-unused-vars': 2,
+        'no-trailing-spaces': 2,
+        'object-curly-spacing': [2, 'never'],
+        'no-spaced-func': 2,
+        'space-before-function-paren': [2, {
+            'anonymous': 'always',
+            'named': 'never',
+            'asyncArrow': 'always',
+        }],
+        'generator-star-spacing': [2, {'before': false, 'after': true}],
+    },
 };
---
+++ README.md
@@ -0,0 +1,17 @@
+### Install
+```text
+npm install @anvilapp/react-native-bottom-sheet --save
+```
+or
+```text
+yarn add @anvilapp/react-native-bottom-sheet
+```
+
+### Usage example
+```js
+import BottomSheet from '@anvilapp/react-native-bottom-sheet';
+
+<BottomSheet visible={true}>
+    <Text>Hi!</Text>
+</BottomSheet>
+```
